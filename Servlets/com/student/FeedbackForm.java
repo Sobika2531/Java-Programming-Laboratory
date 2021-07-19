e18042cc7254f9ad9b5e9ff29b107dc79a715510
Sobika Senthilnathan <76904383+Sobika2531@users.noreply.github.com>
2021-07-19 13:51:55 +0530

    Create FeedbackForm.java

---
+++ Servlets/com/student/FeedbackForm.java
@@ -0,0 +1,11 @@
+package com.student;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.*;
+
+public class FeedbackForm extends HttpServlet {
+	
+	public void service(HttpServletRequest req, HttpServletResponse res) {
+		
+	}
+}

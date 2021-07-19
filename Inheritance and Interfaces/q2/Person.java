7efc1a740c666ab06b1bb89fe0418bba92051f44
Sobika Senthilnathan <76904383+Sobika2531@users.noreply.github.com>
2021-07-19 12:28:52 +0530

    Create Person.java

---
+++ Inheritance and Interfaces/q2/Person.java
@@ -0,0 +1,35 @@
+package q2;
+
+public abstract class Person {
+	protected String name;
+	protected int age;
+	protected double height;
+	protected double weight;
+	
+	
+	Person(String name,int age,double height,double weight)   //constructor with four arguments
+	{
+		this.name=name;
+		this.age=age;
+		this.height=height;
+		this.weight=weight;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public double getHeight() {
+		return height;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	
+	public void to_String()
+	{
+		System.out.print("\nname: "+name+"\nage: "+age+"\nheight: "+height+"\nweight: "+weight);
+	}
+}

142610e8e712a1daad348ef14057280686b9d766
Sobika Senthilnathan <76904383+Sobika2531@users.noreply.github.com>
2021-07-19 11:30:30 +0530

    Create One.java

---
+++ Array of Objects/2/One.java
@@ -0,0 +1,22 @@
+package tues08;
+
+public class One {
+	public int x=30;
+	int y=15;
+	private int s=67;
+	
+	public void u()
+	{
+		System.out.print("hi! i am from class One and public method u()\n");
+	}
+	
+	void w()
+	{
+		System.out.print("hi! i am from class One and default method w()\n");
+	}
+	
+	private void v()
+	{
+		System.out.print("hi! i am from class One and private method v()\n");
+	}
+}

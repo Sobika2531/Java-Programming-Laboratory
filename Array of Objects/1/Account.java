133fd3bf4c16e271a7d97980ad9de840417da560
Sobika Senthilnathan <76904383+Sobika2531@users.noreply.github.com>
2021-07-19 11:28:55 +0530

    Create Account.java

---
+++ Array of Objects/1/Account.java
@@ -0,0 +1,57 @@
+package tues08;
+
+public class Account {
+	final static double interest=0.06;
+	String name;
+	double balance;
+	int acc_number;
+	
+	public Account()
+	{
+		name="";
+		balance=0.0;
+		acc_number=0;
+	}
+	
+	public Account(String s,double b,int a)
+	{
+		name=s;
+		balance=b;
+		acc_number=a;
+	}
+
+	public void setName(String str)
+	{
+		this.name=str;
+	}
+
+	public void setBalance(double bal)
+	{
+		this.balance=bal;
+	}
+	
+	public void setAccount(int acc)
+	{
+		this.acc_number=acc;
+	}
+	
+	public double getBalance()
+	{
+		return balance;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getAccount()
+	{
+		return acc_number;
+	}
+	
+	public static double getInterest()
+	{
+		return interest;
+	}
+}

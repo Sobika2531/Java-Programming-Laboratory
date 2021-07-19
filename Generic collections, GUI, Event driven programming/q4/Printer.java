85e997447cd197355179f730fdbea563e20c5245
Sobika Senthilnathan <76904383+Sobika2531@users.noreply.github.com>
2021-07-19 13:13:20 +0530

    Create Printer.java

---
+++ Generic collections, GUI, Event driven programming/q4/Printer.java
@@ -0,0 +1,15 @@
+package q4;
+
+import javax.swing.*;
+
+public class Printer {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+			PrinterTest p=new PrinterTest();
+			p.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			p.setSize(550,250);
+			p.setVisible(true);
+	}
+
+}

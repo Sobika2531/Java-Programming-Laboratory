aa6b463cc2f0d83da66ece3d32fc76077685681d
Sobika Senthilnathan <76904383+Sobika2531@users.noreply.github.com>
2021-07-19 13:12:04 +0530

    Create GuessGame.java

---
+++ Generic collections, GUI, Event driven programming/q3/GuessGame.java
@@ -0,0 +1,13 @@
+package q3;
+
+import javax.swing.JFrame;
+public class GuessGame {
+
+	public static void main(String[] args) {
+		 Test guessgame = new Test();
+		 		 guessgame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		         guessgame.setSize(450, 450);
+		         guessgame.setVisible(true);
+
+	}
+}
